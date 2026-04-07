1a7de1906bee0c047c4247d69fc879438d360018
sergiomerces <sergio.merces@gmail.com>
2026-04-07 02:11:58 -0300

    editando informações de pesquisa do Google

---
+++ assets/js/schema.js
@@ -0,0 +1,8 @@
+const schema = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    "name": "Sergio Mercês",
+    "url": "https://sergiomerces.github.io",
+    "image": "https://sergiomerces.github.io/assets/img/profile.jpg",
+    "description": "Sérgio Mercês, graduando em Tecnologia em Desenvolvimento Web pela Faculdade Anhanguera, com experiência em HTML5, CSS3, JavaScript, Node.js, Python, SQL e frameworks modernos. Apaixonado por tecnologia, cultura geek e projetos colaborativos"
+};
